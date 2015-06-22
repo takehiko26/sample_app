95f5d7499241b91f2ac8456e9f6c1ba56e0596bf
takehiko26 <takehiko.hara26@gmail.com>
2015-06-22 23:13:30 +0900

    Chapter-5

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
+require File.expand_path('../boot', __FILE__)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,5 +25,6 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,22 +2,60 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
+end
+
+=begin
+describe "Static pages" do
+
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
+      # visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
+      # visit '/static_pages/home'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
+      # visit '/static_pages/home'
       expect(page).not_to have_title("| Home")
     end
 
@@ -26,12 +64,14 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
+      # visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
+      # visit '/static_pages/help'
       expect(page).to have_title("#{base_title} | Help")
     end
   end
@@ -39,12 +79,14 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
+      # visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
+      # visit '/static_pages/about'
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
@@ -52,13 +94,16 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
+      # visit '/static_pages/contact'
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
+      # visit '/static_pages/contact'
       expect(page).to have_title("#{base_title} | Contact")
-    end
+  end 
   end
 end
+=end
